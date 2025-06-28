a6ed2927d809c52d762bd6bc8540ab405307654f
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2025-06-28 16:24:13 +0300

    [scanner] Move inside lib and use config

--- lib/scanner.py
+++ lib/scanner.py
@@ -1,64 +1,31 @@
 import json
-import tomllib
 from collections import defaultdict
 from datetime import datetime
 from typing import Any
 
 from playwright.sync_api import BrowserContext, Error, Page, expect, sync_playwright
 
-from lib.globals import *
-from lib.time import time_tracker
+from lib.configs import ScannerConfig
+from lib.globals import QuestionSets, Questions
 
-CONFIG_FILE = 'config.toml'
-"""
-Path to a configuration file with login information.
-"""
-
-QUESTIONS_FILE = 'questions.json'
-"""
-Path to a file with questions.
-"""
-
-CATEGORIES = {'Методы инженерии и анализа данных'}
-"""
-Which categories to scan. Can be set to ``None`` to scan all of them.
-"""
-
-SUBCATEGORIES = {}
-"""
-Which subcategories to scan. Can be set to ``None`` to scam all of them.
-"""
-
-TOPICS = {}
-"""
-Which topics to scan. Can be set to ``None`` to scam all of them.
-"""
-
-TIMES_PER_TOPIC = 30
-"""
-How many times scan every topic.
-"""
-
-with open(CONFIG_FILE, 'rb') as _f:
-    CONFIG = tomllib.load(_f)
-    del _f
+type TopicHierarchy = dict[str, dict[str, list[str]]]
 
 
-def open_laba_ai(context: BrowserContext, /) -> Page:
+def open_laba_ai(context: BrowserContext, config: ScannerConfig, /) -> Page:
     """
     Opens Laba.AI platform and logs in.
     """
     page = context.new_page()
-    page.goto(CONFIG['url'])
-    page.get_by_placeholder('Name Surname', exact=True).fill(CONFIG['full_name'])
+    page.goto(config.url)
+    page.get_by_placeholder('Name Surname', exact=True).fill(config.full_name)
     emails = page.get_by_placeholder('Use corporate email address only', exact=True)
-    emails.first.fill(CONFIG['email'])
-    emails.last.fill(CONFIG['coordinator_email'])
+    emails.first.fill(config.email)
+    emails.last.fill(config.coordinator_email)
     page.get_by_text('Verify', exact=True).click()
     return page
 
 
-def get_topics(page: Page, /) -> dict[str, dict[str, list[str]]]:
+def get_topics(page: Page, /) -> TopicHierarchy:
     """
     Extracts topic hierarchy from the page.
     """
@@ -76,9 +43,9 @@ def get_topics(page: Page, /) -> dict[str, dict[str, list[str]]]:
     return topics
 
 
-def read_existing_questions(filepath: str, topics: dict[str, dict[str, list[str]]], /) -> Questions:
+def read_existing_questions(filepath: str, topics: TopicHierarchy, /) -> Questions:
     """
-    Reads the give JSON with recorded questions, adds missing topics
+    Reads the given JSON with recorded questions, adds missing topics
     and converts question lists into sets.
     """
     with open(filepath) as f:
@@ -127,18 +94,26 @@ def get_question_text(page: Page, /) -> str:
         .locator('[class*="text-sm text-balance select-none pointer-events-none"]')
         .inner_text()
     )
+    # Some questions are identically the same, but use 'е' instead of 'ё'.
+    # Better to replace 'ё' completely to avoid trivial duplicates.
     text = text.replace('ё', 'е').strip('.')
     text = ' '.join(text.split())
     return text
 
 
-def record_questions(context: BrowserContext, topic_name: str, q_sets: QuestionSets, /) -> bool:
+def record_questions(
+        context: BrowserContext,
+        config: ScannerConfig,
+        topic: str,
+        q_sets: QuestionSets,
+        /,
+        ) -> bool:
     """
     Opens Laba.AI, resets topic selection, selects the given topic and starts an exam.
     Then records questions encountered to the given ``q_sets``.
     Returns ``True`` on success and ``False`` otherwise.
     """
-    with open_laba_ai(context) as page:
+    with open_laba_ai(context, config) as page:
         # Remove default selection by clicking the first category.
         # .check() or .uncheck() do not work on the first checkbox as it is in a mixed state.
         # They also often throw an error for an unknown reason when used of the first category.
@@ -146,7 +121,7 @@ def record_questions(context: BrowserContext, topic_name: str, q_sets: QuestionS
         first_category.click()
 
         # Select necessary topic
-        page.get_by_text(topic_name, exact=True).get_by_role('checkbox').click()
+        page.get_by_text(topic, exact=True).get_by_role('checkbox').click()
         # Create assessment
         page.get_by_text('Create assessment', exact=True).click()
         # Start exam
@@ -160,10 +135,10 @@ def record_questions(context: BrowserContext, topic_name: str, q_sets: QuestionS
             .inner_text()
             .strip()
         )
-        if topic_name != actual_topic:
+        if topic != actual_topic:
             print(
                 f'{datetime.now()}\t'
-                f'Actual topic name is {actual_topic!r} instead of {topic_name!r}'
+                f'Actual topic name is {actual_topic!r} instead of {topic!r}'
                 )
             return False
 
@@ -180,7 +155,13 @@ def record_questions(context: BrowserContext, topic_name: str, q_sets: QuestionS
         return True
 
 
-def main() -> None:
+def start_scanner(config: ScannerConfig, /) -> None:
+    questions_filepath = config.output_filepath
+    categories = config.categories
+    subcategories = config.subcategories
+    topics = config.topics
+    times_per_topic = config.times_per_topic
+
     with (
         sync_playwright() as p,
         # Cannot get questions in headless mode
@@ -190,32 +171,30 @@ def main() -> None:
         context.new_page(),
         ):
         # Get existing topics
-        with open_laba_ai(context) as page:
-            topics = get_topics(page)
+        with open_laba_ai(context, config) as page:
+            topic_hierarchy = get_topics(page)
 
         # Load existing questions
-        questions = read_existing_questions(QUESTIONS_FILE, topics)
+        questions = read_existing_questions(questions_filepath, topic_hierarchy)
         try:
-            # Iterate over questions
-            for category, category_dict in topics.items():
-                if CATEGORIES and category not in CATEGORIES: continue
+            # Iterate over existing topic hierarchy
+            for category, category_dict in topic_hierarchy.items():
+                if categories and category not in categories: continue
 
-                for subcategory, topics in category_dict.items():
-                    if SUBCATEGORIES and subcategory not in SUBCATEGORIES: continue
+                for subcategory, topic_list in category_dict.items():
+                    if subcategories and subcategory not in subcategories: continue
 
-                    for topic_name in topics:
-                        if TOPICS and topic_name not in TOPICS: continue
+                    for topic in topic_list:
+                        if topics and topic not in topics: continue
 
-                        q_sets = questions[category][subcategory][topic_name]
-                        for _ in range(TIMES_PER_TOPIC):
+                        q_sets = questions[category][subcategory][topic]
+                        for _ in range(times_per_topic):
                             try:
-                                record_questions(context, topic_name, q_sets)
+                                record_questions(context, config, topic, q_sets)
                             except Error as e:
                                 print(f'{datetime.now()}\t{e}')
         finally:
-            save_questions(questions, QUESTIONS_FILE)
+            save_questions(questions, questions_filepath)
 
 
-if __name__ == '__main__':
-    with time_tracker():
-        main()
+__all__ = 'start_scanner',
