cc9a6eff1bc7f764274c694cd5226973259fe624
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2025-08-28 16:51:03 +0300

    Add script for querying

---
+++ query.py
@@ -0,0 +1,46 @@
+def main() -> None:
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(
+        description='A script for querying analyzed questions',
+        add_help=False,
+        )
+
+    parser.add_argument(
+        'config_file',
+        nargs='?',
+        default='config.toml',
+        help='Path to the configuration file. Defaults to "config.toml".',
+        )
+
+    parser.add_argument(
+        '-h',
+        '--help',
+        action='help',
+        help='If specified, the script shows this help message and exits.',
+        )
+
+    args = parser.parse_args()
+
+    from logging import getLogger
+    from lib.configs import read_config
+    from lib.time import time_tracker
+
+    config = read_config(args.config_file).querier
+    logger = getLogger('script')
+
+    with time_tracker('The script is stopped. Time elapsed: {}', logger.info):
+        logger.info('Press Ctrl+C to stop this script')
+
+        from lib.querier import query
+
+        try:
+            query(config)
+        except KeyboardInterrupt:
+            logger.info('Ctrl+C received, stopping the script...')
+        except Exception:
+            logger.exception('An unknown error has occurred, the script will be stopped shortly')
+
+
+if __name__ == '__main__':
+    main()
